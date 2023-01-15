137838890286cd559b18127816efb02073ebdef7
mauro silva <mauro1400@outlook.es>
2023-01-14 20:14:15 -0400

    permission

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MapScreen extends StatelessWidget {
+
+
+const MapScreen ({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome to the MapScreen'),
+      ),
+    );
+  }
+}
